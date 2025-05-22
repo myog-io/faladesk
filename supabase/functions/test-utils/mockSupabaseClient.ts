9b7c6e112bc262970d8f93eab637421553bedc96
Paulo <p.peresjr@gmail.com>
2025-05-22 19:46:20 -0300

    Add route-to-ai edge function and tests

--- supabase/functions/test-utils/mockSupabaseClient.ts
+++ supabase/functions/test-utils/mockSupabaseClient.ts
@@ -1,8 +1,64 @@
-export function createMockClient() {
+export function createMockClient(data: {
+  conversation?: any
+  messages?: any[]
+  user?: any
+  organization?: any
+} = {}) {
   return {
-    from: () => ({
-      insert: () => ({ select: () => Promise.resolve({ data: {} }) }),
-      select: () => ({ single: () => Promise.resolve({ data: {} }) })
-    })
+    from(table: string) {
+      switch (table) {
+        case 'conversations':
+          return {
+            select: () => ({
+              eq: () => ({
+                single: () => Promise.resolve({ data: data.conversation })
+              })
+            })
+          }
+        case 'users':
+          return {
+            select: () => ({
+              eq: () => ({
+                single: () => Promise.resolve({ data: data.user })
+              })
+            })
+          }
+        case 'messages':
+          return {
+            select: () => ({
+              eq: () => ({
+                order: () => ({
+                  limit: () => Promise.resolve({ data: data.messages })
+                }),
+                single: () => Promise.resolve({ data: data.messages?.[0] })
+              })
+            }),
+            insert: (vals: any) => ({
+              select: () =>
+                Promise.resolve({ data: { id: 'new-id', ...vals } }),
+              single: () =>
+                Promise.resolve({ data: { id: 'new-id', ...vals } })
+            })
+          }
+        case 'organizations':
+          return {
+            select: () => ({
+              eq: () => ({
+                single: () => Promise.resolve({ data: data.organization })
+              })
+            })
+          }
+        default:
+          return {
+            select: () => ({ single: () => Promise.resolve({ data: {} }) })
+          }
+      }
+    },
+    channel() {
+      return {
+        send: async () => {},
+        unsubscribe: async () => {}
+      }
+    }
   }
 }
