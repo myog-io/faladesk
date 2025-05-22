a9c2f9ab07312bb733a5dc2107dc7871ab0ad2b7
Paulo <p.peresjr@gmail.com>
2025-05-22 20:38:50 -0300

    Making some shit work

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',     // Exposes the dev server externally (e.g. in Docker)
+    port: 3000,           // Optional: match your Docker port
+    strictPort: true,     // Fail if port is taken
+    watch: {
+      usePolling: true,   // Fixes file change detection in volume mounts
+    },
+  }
 })
