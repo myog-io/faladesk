98678b604b4c888dbcf507b6e3f725dd66b1cc50
Paulo <p.peresjr@gmail.com>
2025-05-23 10:52:29 -0300

    fix shims and add login test

--- web/shims/gluestack-style-react.mjs
+++ web/shims/gluestack-style-react.mjs
@@ -1,1 +1,31 @@
-export default {}
+// Shim for `@gluestack-style/react`. Provides the minimal APIs required by
+// `@gluestack-ui/themed` so the project can run in a browser environment.
+import React from 'react'
+
+// Simple forwarder component used by gluestack UI
+export const AsForwarder = React.forwardRef(({ as: Comp = 'div', ...props }, ref) =>
+  React.createElement(Comp, { ref, ...props })
+)
+
+// Extremely light "styled" implementation that simply returns the base component
+export const styled = (Component) =>
+  React.forwardRef((props, ref) => React.createElement(Component, { ref, ...props }))
+
+// No-op providers and helpers
+export const StyledProvider = ({ children }) => <>{children}</>
+export const useStyled = () => ({})
+export const flush = () => {}
+export const createConfig = () => ({})
+export const Theme = {}
+export const useTheme = () => ({})
+
+export default {
+  AsForwarder,
+  styled,
+  StyledProvider,
+  useStyled,
+  flush,
+  createConfig,
+  Theme,
+  useTheme,
+}
