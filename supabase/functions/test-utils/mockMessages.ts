d41c8e890655a0edb0b5b5e445383e92c3326d3b
Paulo <p.peresjr@gmail.com>
2025-09-27 10:06:49 -0300

    re-start with the concept

--- supabase/functions/test-utils/mockMessages.ts
+++
@@ -1,5 +0,0 @@
-export const sampleMessage = {
-  customer_name: 'Tester',
-  channel: 'whatsapp',
-  content: 'hello'
-}
--- web/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  testMatch: ['<rootDir>/../apps/frontend/tests/**/*.(test|spec).tsx?'],
-};
